7f675800254833aa3a62b276168bed124141eafd
chelsea <2215663076@qq.com>
2023-06-28 15:19:11 +0800

    support resnet50

--- model.py
+++ model.py
@@ -3,11 +3,18 @@ import torch
 from torch.nn import functional as F
 
 class LinearLayer(nn.Module):
-    def __init__(self, dim_in, dim_out, k):
+    def __init__(self, dim_in, dim_out, k, model):
         super(LinearLayer, self).__init__()
-        self.fc = nn.ModuleList([nn.Linear(dim_in, dim_out) for i in range(k)])
+        if 'ViT' in model:
+            self.fc = nn.ModuleList([nn.Linear(dim_in, dim_out) for i in range(k)])
+        else:
+            self.fc = nn.ModuleList([nn.Linear(dim_in * 2 ** (i + 2), dim_out) for i in range(k)])
 
     def forward(self, tokens):
         for i in range(len(tokens)):
-            tokens[i] = self.fc[i](tokens[i][:, 1:, :])
+            if len(tokens[i].shape) == 3:
+                tokens[i] = self.fc[i](tokens[i][:, 1:, :])
+            else:
+                B, C, H, W = tokens[i].shape
+                tokens[i] = self.fc[i](tokens[i].view(B, C, -1).permute(0, 2, 1).contiguous())
         return tokens
