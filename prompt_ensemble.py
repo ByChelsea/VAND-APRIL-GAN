7a2f14c8df3a9858daf60df3665f2f2317cc02b9
chelsea <2215663076@qq.com>
2023-07-05 16:36:38 +0800

    faster

--- prompt_ensemble.py
+++ prompt_ensemble.py
@@ -5,17 +5,17 @@ import torch
 import numpy as np
 
 
-def encode_text_with_prompt_ensemble(model, texts, tokenizer, device):
+def encode_text_with_prompt_ensemble(model, objs, tokenizer, device):
     prompt_normal = ['{}', 'flawless {}', 'perfect {}', 'unblemished {}', '{} without flaw', '{} without defect', '{} without damage']
     prompt_abnormal = ['damaged {}', 'broken {}', '{} with flaw', '{} with defect', '{} with damage']
     prompt_state = [prompt_normal, prompt_abnormal]
     prompt_templates = ['a bad photo of a {}.', 'a low resolution photo of the {}.', 'a bad photo of the {}.', 'a cropped photo of the {}.', 'a bright photo of a {}.', 'a dark photo of the {}.', 'a photo of my {}.', 'a photo of the cool {}.', 'a close-up photo of a {}.', 'a black and white photo of the {}.', 'a bright photo of the {}.', 'a cropped photo of a {}.', 'a jpeg corrupted photo of a {}.', 'a blurry photo of the {}.', 'a photo of the {}.', 'a good photo of the {}.', 'a photo of one {}.', 'a close-up photo of the {}.', 'a photo of a {}.', 'a low resolution photo of a {}.', 'a photo of a large {}.', 'a blurry photo of a {}.', 'a jpeg corrupted photo of the {}.', 'a good photo of a {}.', 'a photo of the small {}.', 'a photo of the large {}.', 'a black and white photo of a {}.', 'a dark photo of a {}.', 'a photo of a cool {}.', 'a photo of a small {}.', 'there is a {} in the scene.', 'there is the {} in the scene.', 'this is a {} in the scene.', 'this is the {} in the scene.', 'this is one {} in the scene.']
 
-    text_features_batch = []
-    for j in range(len(texts)):
+    text_prompts = {}
+    for obj in objs:
         text_features = []
         for i in range(len(prompt_state)):
-            prompted_state = [state.format(texts[j]) for state in prompt_state[i]]
+            prompted_state = [state.format(obj) for state in prompt_state[i]]
             prompted_sentence = []
             for s in prompted_state:
                 for template in prompt_templates:
@@ -28,8 +28,6 @@ def encode_text_with_prompt_ensemble(model, texts, tokenizer, device):
             text_features.append(class_embedding)
 
         text_features = torch.stack(text_features, dim=1).to(device)
-        text_features_batch.append(text_features)
+        text_prompts[obj] = text_features
 
-    text_features_batch = torch.stack(text_features_batch, dim=0)
-
-    return text_features_batch
+    return text_prompts
